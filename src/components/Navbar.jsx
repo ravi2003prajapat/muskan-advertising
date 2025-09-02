4c3105213db940864072a0ec3b6660bfdb486be4
RaviPrajapat007 <120958642+RaviPrajapat007@users.noreply.github.com>
2025-09-02 13:00:51 +0530

    responsive home page

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,35 +1,60 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import styles from "./Navbar.module.css";
 import logo from "../assets/Logo.webp";
 
 const Navbar = () => {
+  const [isMobileMenuOpen, setIsMobileMenuOpen] = useState(false);
+
   const scrollToContact = () => {
     const contactSection = document.getElementById("contact");
     if (contactSection) {
       contactSection.scrollIntoView({ behavior: "smooth" });
     }
+    setIsMobileMenuOpen(false); // Close mobile menu after click
   };
 
   return (
-    <div className={styles.navbar}>
+    <nav className={styles.navbar}>
       <div className={styles.logo}>
         <img src={logo} alt="Logo" />
       </div>
-      <div className={styles.right}>
+
+      <div
+        className={styles.hamburger}
+        onClick={() => setIsMobileMenuOpen(!isMobileMenuOpen)}
+      >
+        <span className={isMobileMenuOpen ? styles.barActive : styles.bar}></span>
+        <span className={isMobileMenuOpen ? styles.barActive : styles.bar}></span>
+        <span className={isMobileMenuOpen ? styles.barActive : styles.bar}></span>
+      </div>
+
+      <div className={`${styles.right} ${isMobileMenuOpen ? styles.active : ""}`}>
         <ul>
           <li>
-            <Link to="/" className={styles.underline}>
+            <Link
+              to="/"
+              className={styles.underline}
+              onClick={() => setIsMobileMenuOpen(false)}
+            >
               Home
             </Link>
           </li>
           <li>
-            <Link to="/about" className={styles.underline}>
+            <Link
+              to="/about"
+              className={styles.underline}
+              onClick={() => setIsMobileMenuOpen(false)}
+            >
               About Us
             </Link>
           </li>
           <li>
-            <Link to="/services" className={styles.underline}>
+            <Link
+              to="/services"
+              className={styles.underline}
+              onClick={() => setIsMobileMenuOpen(false)}
+            >
               Services
             </Link>
           </li>
@@ -40,7 +65,7 @@ const Navbar = () => {
           </li>
         </ul>
       </div>
-    </div>
+    </nav>
   );
 };
 
